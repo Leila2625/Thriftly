c3d25d82e6671b37c5d68e56dc5872429f9e3ba2
savannahtav <156825992+savannahtav@users.noreply.github.com>
2025-04-07 17:15:27 -0400

    Create order_items.js

---
+++ server/routes/order_items.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Placeholder route
+router.get('/', (req, res) => {
+  res.send('order_items route placeholder');
+});
+
+module.exports = router;
